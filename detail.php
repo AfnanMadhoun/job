f8b1773491538a61ed3c401eef6a7097b1671067
Afnan Madhoun <161048@ppu.edu.ps>
2019-11-11 23:47:01 +0200

    don't take id

--- detail.php
+++ detail.php
@@ -1,29 +1,103 @@
 <?php
 require 'connect.php';
 
-if (!isset($_GET['id']) or $_GET['id'] == ""){
-    header("location:main.php");
+// $result1= mysqli_query($conn,$query1);
+$id =$_GET['id'];
+
+if (isset($_GET['id']))
+{
+    $query = "SELECT * FROM jobsoffering WHERE id ='$id'";
+    $result = mysqli_query($conn,$query);
+    $row = mysqli_fetch_assoc($result);
+    // $row = mysqli_fetch_assoc($result);
+
+
+// $id = $_GET['id'];
+// $id_stores = $_POST['id_stores'];
+// $name = $_POST['name'];
+// $original_price = $_POST['original_price'];
+// $discount_prince = $_POST['discount_prince'];
+
 }
-$id = $_GET['id'];
+
+echo "</br>";
+echo "</br>";
+
+echo "id :" . " $row[id]";
+echo "</br>";
+
+echo "id stores:". "$row[jobname]";
+echo "</br>";
+echo  "name item :" ."$row[image]";
+echo "</br>";
 
 
-$query2 = "SELECT * FROM jobsoffering WHERE id='$id'";
 
-$result2 = mysqli_query($conn, $query2);
 
-// if (mysqli_num_rows($result1)==0){
+
+
+
+
+// if (!isset($_GET['id']) or $_GET['id'] == ""){
 //     header("location:main.php");
 // }
+// $id = $_GET['id'];
 
-$jobs = mysqli_fetch_assoc($result2);
 
-echo "test";
-echo "<ul>";
-echo "<li>" . $jobs['jobname'] ."test". "</li>";
-echo "<li>" . $jobs['respons'] . "</li>";
-echo "<li>" . $jobs['image'] ."<img src= 'default.jpg'>". "</li>";
+// $query2 = "SELECT * FROM jobsoffering WHERE id='$id'";
 
-echo "</ul>";
+// $result2 = mysqli_query($conn, $query2);
+
+// // if (mysqli_num_rows($result1)==0){
+// //     header("location:main.php");
+// // }
+
+// $jobs = mysqli_fetch_assoc($result2);
+
+// echo "test";
+// echo "<ul>";
+// echo "<li>" . $jobs['jobname'] ."test". "</li>";
+// echo "<li>" . $jobs['respons'] . "</li>";
+// echo "<li>" . $jobs['image'] ."<img src= 'default.jpg'>". "</li>";
+
+// echo "</ul>";
+
+
+
+// // $result1= mysqli_query($conn,$query1);
+// $id =$_GET['id'];
+// if (isset($_GET['id']))
+// {
+//     $query = "SELECT * FROM stores WHERE id =$id";
+//     $result = mysqli_query($conn,$query);
+//    $row = mysqli_fetch_assoc($result);
+
+// // $id = $_GET['id'];
+// // $id_stores = $_POST['id_stores'];
+// // $name = $_POST['name'];
+// // $original_price = $_POST['original_price'];
+// // $discount_prince = $_POST['discount_prince'];
+// }
+
+
+// echo "</br>";
+// echo "</br>";
+
+// echo "id :" . " $row[id]";
+// echo "</br>";
+
+// echo "id stores:". "$row[id_stores]";
+// echo "</br>";
+// echo  "name item :" ."$row[name]";
+// echo "</br>";
+// echo  "originel price :"."$row[original_price]";
+// echo "</br>";
+// echo "discount_prince :" ."$row[discount_prince]";
+// echo "</br>";
+
+
+
+
 
 
 
